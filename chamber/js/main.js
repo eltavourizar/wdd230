295dd3b1b368403c53ba8d3538a49fe920e0635d
eltavourizar <u432027012@byui.edu>
2022-02-08 20:54:56 -0600

    adding the date in the header

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -16,3 +16,19 @@ copyrightYear.textContent = d.getFullYear();
 const lastUpdate = document.getElementById("lastUpdate")
 lastUpdate.textContent = document.lastModified;
 
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
